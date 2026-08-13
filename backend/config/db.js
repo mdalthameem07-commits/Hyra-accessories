a5b0aad847b660df0f847fd25eba9e471f8fe5b8
Mohamed Al Thameem <mdalthameem07@gmail.com>
2026-08-13 09:44:54 +0530

    update the database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://hyrauser:0IAgNnXTL3OrXLr1@cluster01.7lqkekt.mongodb.net/?appName=Cluster01");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
